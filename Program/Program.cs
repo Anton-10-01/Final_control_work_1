1d62c2634d52be7536e12a927f965ea358538a90
Anton <razjivin.anton@yandex.ru>
2023-08-03 18:53:45 +0300

    Добавление кавычек при выводе

--- Program/Program.cs
+++ Program/Program.cs
@@ -41,8 +41,8 @@ string[] ArraySort(string[] arr, int numrep) {
 void ArrayPrint(string[] arr) {
     Console.Write("[");
     for (int i = 0; i < arr.Length; i++) {
-            if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
-            else Console.Write($"{arr[i]}");
+            if (i < arr.Length - 1) Console.Write($"\"{arr[i]}\", ");
+            else Console.Write($"\"{arr[i]}\"");
     }
     Console.WriteLine("]");
 }
